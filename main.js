ec02bf45de00c354adcfde9fdd92a0fb216fd809
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-07-30 00:50:53 +0100

    ...

---
+++ main.js
@@ -0,0 +1,7 @@
+var fullname = document.getElementById("name");
+var height = document.getElementById("height");
+var nation = document.getElementById("country");
+
+fullname.innerHTML = "My name is Oluwabunmi Gbemisoye.";
+height.innerHTML = "I am 6 feet tall.";
+nation.innerHTML = "Nigeria is my country.";
